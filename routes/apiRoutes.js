3f0a0b86782e252d6547bb051771ecb35e0d7879
Shao Ge <geshao3606@gmail.com>
2021-06-05 10:57:29 -0700

    server setup

---
+++ routes/apiRoutes.js
@@ -0,0 +1,48 @@
+const Workout = require("../models/workout.js");
+const router = require("express").Router();
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", ({ params, body }, res) => {
+    Workout.findOneAndUpdate(
+        { _id: params.id },
+        { $push: { exercises: body } },
+        { new: true }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
